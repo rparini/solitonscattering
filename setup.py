42d205d1ac2fb229c3ec5661b600b731a74e5433
Robert Parini <robert.parini@gmail.com>
2018-09-26 15:19:30 +0100

    Update setup.py
    Require cython

--- setup.py
+++ setup.py
@@ -1,42 +1,23 @@
 #!/usr/bin/env python
 import os
-import sys
-import shutil
-import unittest
-from distutils.core import setup, Command
+from distutils.core import setup
 from distutils.extension import Extension
-from numpy.distutils.misc_util import get_numpy_include_dirs
-import warnings
 import numpy as np
 
-try:
-    from Cython.Distutils import build_ext
-    from Cython.Build import cythonize
-except ImportError:
-    use_cython = False
-else:
-    use_cython = True
-
-print('use_cython =', use_cython)
-if use_cython:
-    cmdclass = {'build_ext': build_ext}
-else:
-    cmdclass = {}
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
 import os
 path = os.path.dirname(os.path.abspath(__file__))
 
-ext = '.pyx' if use_cython else '.cpp'
-
-ext_modules = [Extension("SolitonScattering.CUtilities_caller", [path+"/SolitonScattering/CUtilities_caller"+ext], 
+ext_modules = [Extension("SolitonScattering.CUtilities_caller", [path+"/SolitonScattering/CUtilities_caller.pyx"], 
     language = "c++", 
-    include_dirs = [np.get_include()])]
-
+    include_dirs = [np.get_include()],
+)]
 
 packages = [
     'SolitonScattering',
 ]
-
 
 # configure setup
 setup(
@@ -44,6 +25,7 @@ setup(
     author = 'Robert Parini',
     cmdclass = cmdclass,
     ext_modules = ext_modules,
+    install_requires = ['cython', 'numpy', 'scipy', 'xarray'],
     packages = packages,
     platforms = ['all']
 )
