61ffba6022fd5e46bab23c2015ba5c11b4ca09ba
Robert Parini <robert.parini@gmail.com>
2018-09-26 15:42:29 +0100

    Make cython optional

--- setup.py
+++ setup.py
@@ -4,16 +4,21 @@ from distutils.core import setup
 from distutils.extension import Extension
 import numpy as np
 
-from Cython.Distutils import build_ext
-from Cython.Build import cythonize
-
 import os
 path = os.path.dirname(os.path.abspath(__file__))
 
-ext_modules = [Extension("SolitonScattering.CUtilities_caller", [path+"/SolitonScattering/CUtilities_caller.pyx"], 
-    language = "c++", 
-    include_dirs = [np.get_include()],
-)]
+try:
+    from Cython.Distutils import build_ext
+    from Cython.Build import cythonize
+    cmdclass = {'build_ext': build_ext}
+
+    ext_modules = [Extension("SolitonScattering.CUtilities_caller", [path+"/SolitonScattering/CUtilities_caller.pyx"], 
+        language = "c++", 
+        include_dirs = [np.get_include()],
+    )]
+except ImportError:
+    cmdclass = {}
+    ext_modules = []
 
 packages = [
     'SolitonScattering',
@@ -23,9 +28,9 @@ packages = [
 setup(
     name = 'SolitonScattering',
     author = 'Robert Parini',
-    cmdclass = {'build_ext': build_ext},
+    cmdclass = cmdclass,
     ext_modules = ext_modules,
-    install_requires = ['cython', 'numpy', 'scipy', 'xarray'],
+    install_requires = ['numpy', 'scipy', 'xarray'],
     packages = packages,
     platforms = ['all']
 )
